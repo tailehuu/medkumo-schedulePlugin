e7842f8fd34a9d6db4da8af6f615c5e7c9352e61
Phien Ngo <phien.ngo@terralogic.com>
2016-12-14 16:36:37 +0700

    Add struct source and update sdk file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# medkumo-schedulePlugin
+# Medkumo Javascript SDK
+
+Using this a SDK to book an appointment in Medkumo website.
+
+### Javascript snippet
+You should insert this snippet at the beginning of <body> tag.
+```html
+<body>
+    <script src="//sdk.medkumo.com/medkumo.js"></script>
+    ...
+</body>
+```
+
+### Create a button to book an appointment
+```html
+<button onclick="Medkumo.book('your hospital key', 'your doctor key')">Booking An Appointment</button>
+```
+
+You can see full example in this file example-publisher.html.
---
+++ medkumo.js
@@ -0,0 +1,38 @@
+/*
+ * Medkumo Javascript SDK
+ *
+ */
+(function(window, undefined) {
+    var Medkumo = {},
+        Config = {};
+
+    if (window.Medkumo) {
+        return;
+    }
+
+    Medkumo.book = function(hospitalKey, doctorKey) {
+        console.log('executing book...');
+        var session= new Date();
+        PopupCenter('bookAnAppointment.html?hospitalKey=' + hospitalKey + '&doctorKey=' + doctorKey + '&session=' + session, "Book An Appointment", 650, 500);
+    };
+
+    function PopupCenter(url, title, w, h) {
+        // Fixes dual-screen position                         Most browsers      Firefox
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        var top = ((height / 2) - (h / 2)) + dualScreenTop;
+        var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+            newWindow.focus();
+        }
+    }
+
+    window.Medkumo = Medkumo;
+})(this);
